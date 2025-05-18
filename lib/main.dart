c91fb4482114b27cb602358a51412e4da803ed3c
JingZheee <zhe03@graduate.utm.my>
2025-05-19 00:54:04 +0800

    [Firebase Setup] Initial Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'core/theme/app_theme.dart';
 import 'features/auth/login_screen.dart';
 import 'features/parent/parent_dashboard_screen.dart';
 import 'features/teacher/teacher_dashboard_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
@@ -22,7 +24,7 @@ class NurseryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Nursery App',
+      title: 'Kiddos',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme(),
       home: const AuthenticationWrapper(),
